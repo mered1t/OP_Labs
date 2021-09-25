651c99fda135515659763db625c70684e8d785e5
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2021-09-25 12:38:54 +0300

    Add files via upload

---
+++ ОП лаб 1 c++.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    double pi;
+        pi = 3.1415926535;
+        int angl;
+        std::cout << "Enter the degree measure of the angle= ";
+        std::cin >> angl;
+        double radian;
+        radian = angl * (pi/180);
+        std::cout << "Radian:";
+        std::cout << radian << endl;
+        std::cout << "Press Enter to continue..." << endl;
+        cin.ignore(1, '/ n');
+        cin.get();
+        return 0;
+
+
+
+
+}
+

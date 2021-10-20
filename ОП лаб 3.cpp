ddc3a52192d041744e1c0b5960d746c931168745
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2021-10-20 22:30:34 +0300

    Add files via upload

---
+++ ОП лаб 3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+long long int fact(int i)
+{
+    if (i == 0)
+        return 0;
+    if (i == 1)
+        return 1;
+    return i * fact(i - 1);
+}
+
+int main()
+{
+    int n = 1;
+    double eps = pow(10, -5);
+    float x;
+    float sum = 0;
+
+    std::cout << "Enter the number x: ";
+    std::cin >> x;
+
+    long double A;
+    int f;
+
+
+
+    if (x >= 0 && x <= 2)
+    {
+
+        do {
+
+            f = pow(2, n - 1);
+            std::cout << "n= " << n << endl;
+            A = ((n * pow(x, 2) - 1) / (1 + fact(f)));
+            std::cout << "fact= " << fact(f) << endl;
+            std::cout << "A = " << A << endl;
+            sum += A;
+            n = n + 1;
+
+        } while (A > eps);
+
+        std::cout << "Sum= " << sum;
+    }
+
+    else std::cout << "You have entered wrong number";
+
+    cin.ignore(1, '/n');
+    cin.get();
+}

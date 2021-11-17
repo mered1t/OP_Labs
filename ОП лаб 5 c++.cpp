3144cf4afabdf1a86079cd089d2b7802188ac8b1
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2021-11-17 17:06:36 +0200

    Add files via upload

---
+++ ОП лаб 5 c++.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int counter = 0;
+	int number;
+	std::cout << "Enter the number: ";
+	std::cin >> number;
+
+	int maxCounter = 0;
+	int maxDivN = 1;
+
+	if (number <= 0) { std::cout << "You have entered wrong number";}
+
+	else {
+		for (int i = 1; i <= number; i++) {
+			counter = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					counter++;
+				}
+			}
+			if (counter >= maxCounter) {
+				maxDivN = i;
+				maxCounter = counter;
+			}
+		}
+	}
+	std::cout << "The number with the biggest sum of dividers = " << maxDivN;
+	cin.ignore(1, '/ n');
+	cin.get();
+}

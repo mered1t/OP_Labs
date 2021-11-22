88899ad61e70b2bd93f442abc991a5334af6220e
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2021-11-22 21:00:31 +0200

    Add files via upload

---
+++ OP_Lab6 c++.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+
+
+int sumOfDiv(int num) {
+
+    int accumulator = 0;
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            accumulator += i;
+        }
+    }
+    return accumulator;
+}
+
+int main() {
+
+    int sum = 0;
+    int counter = 0;
+
+    int eps;
+    std::cout << "Enter the number: ";
+    std::cin >> eps;
+
+    while (counter <= eps) {
+        counter++;
+        sum = sumOfDiv(counter);
+        if (counter == sum) continue;
+        if (sumOfDiv(sum) == counter && counter > sum) {
+            std::cout << "Amicable pair: " << sum << " and " << counter << std::endl;
+        }
+    }
+  
+    std::cin.ignore(1, '/n');
+    std::cin.get();
+
+}
